8275e0cb95f15b7947902d71cd9fa629b2b0b820
CreeperMain <creepermaingaming@gmail.com>
2023-07-26 21:29:50 +0200

    added lazy.nvim and telescope

--- init.lua
+++ init.lua
@@ -1,12 +1,19 @@
 require("remaps")
 require("packagemanager")
-
-require("lazy").setup({
+require('lazy').setup({
+-- color scheme
   {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     opts = {},
-  }
-  })
+  },
+--fuzzy finder
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.2',
+-- or                              , branch = '0.1.x',
+      dependencies = { 'nvim-lua/plenary.nvim' },
+    },
+
+})
 vim.cmd[[colorscheme tokyonight-night]]
