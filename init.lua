948ed0e2401d02ba85ee671934f9636504b13ca2
CreeperMain <creepermaingaming@gmail.com>
2023-07-27 21:04:13 +0200

    add lsp and mason

--- init.lua
+++ init.lua
@@ -24,11 +24,46 @@ require('lazy').setup({
       },
       build = ':TSUpdate',
     },
-    'nvim-treesitter/playground',
+    'nvim-treesitter/playground', --mozhda ne trebit
     'ThePrimeagen/harpoon',
     'mbbill/undotree',
     'tpope/vim-fugitive',
     'gentoo/gentoo-syntax',
+    --lsp stuff
+    {
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v2.x',
+      dependencies = {
+        -- LSP Support
+        {'neovim/nvim-lspconfig'},             -- Required
+        {                                      -- Optional
+          'williamboman/mason.nvim',
+          build = function()
+            pcall(vim.api.nvim_command, 'MasonUpdate')
+          end,
+        },
+        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},     -- Required
+        {'hrsh7th/cmp-nvim-lsp'}, -- Required
+        {'L3MON4D3/LuaSnip'},     -- Required
+      },
+    },
+    --statusline
+    {
+      -- Set lualine as statusline
+      'nvim-lualine/lualine.nvim',
+      -- See `:help lualine.txt`
+      opts = {
+        options = {
+          icons_enabled = true,
+          theme = 'ayu_mirage',
+          component_separators = '|',
+          section_separators = '',
+        },
+      },
+    },
 })
 vim.cmd[[colorscheme tokyonight-night]]
 require("treesitter") 
