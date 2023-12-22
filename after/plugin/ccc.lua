d8c24173296afa6608636847da33bfaa72309a82
CreeperMain <creepermaingaming@gmail.com>
2023-12-23 00:05:21 +0100

    added formatting and stuff

---
+++ after/plugin/ccc.lua
@@ -0,0 +1,9 @@
+local ccc = require("ccc")
+ccc.setup({
+	  -- Your preferred settings
+	  -- Example: enable highlighter
+	  highlighter = {
+	    auto_enable = true,
+	    lsp = true,
+	  },
+	})
--- lua/configs/lazy.lua
+++ lua/configs/lazy.lua
@@ -1,154 +1,213 @@
 --boot strapping lazy pkg manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- adding plugins
-require('lazy').setup({
--- color scheme
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
--- tree sitter bit
-{
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-},
-{
-	'nvim-treesitter/playground',
-},
--- lsp stuff
-{
-	'VonHeikemen/lsp-zero.nvim',
-	branch = 'v3.x',
-	dependencies = {
-		--lsp support
-		{'neovim/nvim-lspconfig'},
+require("lazy").setup({
+	-- color scheme
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	-- tree sitter bit
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
+	{
+		"nvim-treesitter/playground",
+	},
+	-- lsp stuff
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
+		dependencies = {
+			--lsp support
+			{ "neovim/nvim-lspconfig" },
 			--mason stuff
-			{'williamboman/mason.nvim'},
-	 		{'williamboman/mason-lspconfig.nvim'},
-            {'jay-babu/mason-nvim-dap.nvim'},
-		--autocompletion
-		{'hrsh7th/cmp-nvim-lsp'},
-		{'hrsh7th/nvim-cmp'},
-		{'hrsh7th/cmp-path'},
-		{'hrsh7th/cmp-buffer'},
-        --snippets
-		{'L3MON4D3/LuaSnip'},
-        {'rafamadriz/friendly-snippets'},
-			}
-},
--- file nav
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
+			{ "jay-babu/mason-nvim-dap.nvim" },
+			--autocompletion
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-path" },
+			{ "hrsh7th/cmp-buffer" },
+			--snippets
+			{ "L3MON4D3/LuaSnip" },
+			{ "rafamadriz/friendly-snippets" },
+		},
+	},
+	-- file nav
 	--harpoon
-	{'ThePrimeagen/harpoon'},
+	{ "ThePrimeagen/harpoon" },
 	--telescope
 	{
-    'nvim-telescope/telescope.nvim', branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    	},
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 	--nvim-tree
-{
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("nvim-tree").setup {}
-  end,
-},
--- git
-  { "kdheepak/lazygit.nvim" },
-  { "airblade/vim-gitgutter" },
--- dap
-  {
-    'mfussenegger/nvim-dap',
-    config = function(_,_)
-        require("configs.keymaps")
-    end
-  },
-  { "rcarriga/nvim-dap-ui",
-  event = "VeryLazy",
-  dependencies = "mfussenegger/nvim-dap",
-  config = function()
-      local dap = require("dap")
-      local dapui = require("dapui")
-      dapui.setup()
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-          dapui.open()
-      end
-      dap.listeners.after.event_terminated["dapui_config"] = function()
-          dapui.close()
-      end
-      dap.listeners.after.event_exited["dapui_config"] = function()
-          dapui.close()
-      end
-  end
-  },
--- auto closing brackets
-{
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    opts = {} -- this is equalent to setup({}) function
-},
-{
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end
-},
--- blankline indentation
-{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
--- neorg
-{
-    "nvim-neorg/neorg",
-    build = ":Neorg sync-parsers",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("neorg").setup {
-        load = {
-          ["core.defaults"] = {}, -- Loads default behaviour
-          ["core.concealer"] = {}, -- Adds pretty icons to your documents
-          ["core.dirman"] = { -- Manages Neorg workspaces
-            config = {
-              workspaces = {
-                notes = "~/Notes",
-              },
-              default_workspace = "notes",
-            },
-          },
-        },
-      }
-    end,
-  },
---misc
-{
-	--gentoo syntax
-'gentoo/gentoo-syntax',
-	--tmux windows switching
-'christoomey/vim-tmux-navigator',
-	--unto tree
-'mbbill/undotree', --look into this
-	--preview color
-'uga-rosa/ccc.nvim',
-	--vim fugitive 
-'tpope/vim-fugitive',
-}
-		})
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("nvim-tree").setup({})
+		end,
+	},
+	-- git
+	{ "kdheepak/lazygit.nvim" },
+	{ "airblade/vim-gitgutter" },
+	-- dap
+	{
+		"mfussenegger/nvim-dap",
+		config = function(_, _)
+			require("configs.keymaps")
+		end,
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		event = "VeryLazy",
+		dependencies = "mfussenegger/nvim-dap",
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+			dapui.setup()
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open()
+			end
+			dap.listeners.after.event_terminated["dapui_config"] = function()
+				dapui.close()
+			end
+			dap.listeners.after.event_exited["dapui_config"] = function()
+				dapui.close()
+			end
+		end,
+	},
+	-- auto closing brackets
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {}, -- this is equalent to setup({}) function
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
+	-- blankline indentation
+	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+	-- neorg
+	{
+		"nvim-neorg/neorg",
+		build = ":Neorg sync-parsers",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("neorg").setup({
+				load = {
+					["core.defaults"] = {}, -- Loads default behaviour
+					["core.concealer"] = {}, -- Adds pretty icons to your documents
+					["core.integrations.treesitter"] = {},
+					["core.completion"] = {
+						config = {
+							engine = "nvim-cmp",
+						},
+					},
+					["core.dirman"] = { -- Manages Neorg workspaces
+						config = {
+							workspaces = {
+								notes = "~/Notes",
+								comp = "~/Notes/computerstuff",
+								long = "~/Notes/longterm",
+								proektna = "~/Notes/proektna",
+							},
+							default_workspace = "notes",
+						},
+					},
+				},
+			})
+		end,
+	},
+	--misc
+	{
+		--gentoo syntax
+		"gentoo/gentoo-syntax",
+		--tmux windows switching
+		"christoomey/vim-tmux-navigator",
+		--unto tree
+		"mbbill/undotree",
+		--preview color
+		"uga-rosa/ccc.nvim",
+		--vim fugitive
+		"tpope/vim-fugitive",
+	},
+	-- md preview
+	{
+		"ellisonleao/glow.nvim",
+		config = true,
+		cmd = "Glow",
+	},
+	-- formatting stuff
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		keys = {
+			{
+				-- Customize or remove this keymap to your liking
+				"<leader>f",
+				function()
+					require("conform").format({ async = true, lsp_fallback = true })
+				end,
+				mode = "",
+				desc = "Format buffer",
+			},
+		},
+		-- Everything in opts will be passed to setup()
+		opts = {
+			-- Define your formatters
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- example: javascript = { { "prettierd", "prettier" } },
+				javascript = { "prettier" },
+				html = { "prettier" },
+				css = { "prettier" },
+				cpp = { "clang_format" },
+				c = { "clang_format" },
+			},
+			-- Set up format-on-save
+			format_on_save = { async = false, timeout_ms = 500, lsp_fallback = true },
+			-- Customize formatters
+			formatters = {
+				shfmt = {
+					prepend_args = { "-i", "2" },
+				},
+			},
+		},
+		init = function()
+			-- If you want the formatexpr, here is the place to set it
+			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+		end,
+	},
+})
